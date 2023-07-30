ee59798613ed7cdf9119d24c1fcc73c38e91c74a
avikshit-30 <avikshit03.jha@gmail.com>
2023-07-30 18:39:45 +0530

    splash also added last commit done

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:architecture_avikshit/features/splash_screen/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/homepage/home_screen.dart';
@@ -10,7 +11,12 @@ final GoRouter router = GoRouter(
   routes: [
     GoRoute(
       path: "/",
-      builder: (context, state) => const LoginScreen(),
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: "/login",
+      name: "login",
+      builder: (context, state) => LoginScreen(),
     ),
     GoRoute(
       path: "/home",
